560e2c079c599a10fcb3f02fa6d49dd56c734271
gandebucuo <54588631+gandebucuo@users.noreply.github.com>
2023-12-06 15:03:15 +0800

    Update route.php

--- src/routes/route.php
+++ src/routes/route.php
@@ -1,12 +1,12 @@
 <?php
-use VirtualCloud\Controllers\ViewController;
+use VirtualCloud\Controllers\RedisController;
 
-Route::get("/",[ViewController::class,'index']);
-Route::get("redis_dbs_index",[ViewController::class,'redis_dbs_index']);
-Route::get("redis_keys_index",[ViewController::class,'redis_keys_index']);
-Route::get("redis_key_show",[ViewController::class,'redis_key_show']);
-Route::get("redis_val_save",[ViewController::class,'redis_val_save']);
-Route::get("redis_val_refresh",[ViewController::class,'redis_val_refresh']);
-Route::get("redis_del",[ViewController::class,'redis_del']);
-Route::get("redis_expire",[ViewController::class,'redis_expire']);
+Route::get("/",[RedisController::class,'index']);
+Route::get("redis_dbs_index",[RedisController::class,'redis_dbs_index']);
+Route::get("redis_keys_index",[RedisController::class,'redis_keys_index']);
+Route::get("redis_key_show",[RedisController::class,'redis_key_show']);
+Route::get("redis_val_save",[RedisController::class,'redis_val_save']);
+Route::get("redis_val_refresh",[RedisController::class,'redis_val_refresh']);
+Route::get("redis_del",[RedisController::class,'redis_del']);
+Route::get("redis_expire",[RedisController::class,'redis_expire']);
 
